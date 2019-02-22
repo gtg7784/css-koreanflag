fc881f7061793fb7ac41f1660640a448b0ab5ab8
Go Tae Geon <gtg7784@lab-c.me>
2019-02-23 01:50:15 +0900

    basic layout

--- src/App.js
+++ src/App.js
@@ -6,6 +6,54 @@ class App extends Component {
     return (
       <div className="App">
         태극기
+        <div className="flag">
+          <div className="bars">
+            <div className="geon">
+              <div className="bar" />
+              <div className="bar" />
+              <div className="bar" />
+            </div>
+            
+            <div className="gam">
+              <div className="bar">
+                <div className="separate" />
+              </div>
+              <div className="bar" />
+              <div className="bar">
+                <div className="separate" />
+              </div>
+            </div>
+          </div>
+
+          <div className="middle">
+            <div className="ovalwrap">
+              <div className="oval" />
+              <div className="oval" />
+            </div>
+          </div>
+
+          <div className="bars">
+            <div className="gon">
+              <div className="bar" />
+              <div className="bar">
+                <div className="separate" />
+              </div>
+              <div className="bar" />
+            </div>
+            <div className="ri">
+              <div className="bar">
+                <div className="separate" />
+              </div>
+              <div className="bar">
+                <div className="separate" />
+              </div>
+              <div className="bar">
+                <div className="separate" />
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     );
   }
